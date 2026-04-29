c4b2d2fa4e221151bc828b8fd2144a198ab78d30
ocscapitale-design <ocscapitale@gmail.com>
2026-04-29 20:47:45 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+var CACHE_NAME = 'ocscapital-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/1776884621819.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
